62f7880b99f48cf1128d76da6ffe6f94883ea9dc
scarrascofuentes <samuel.carrasco.f@mail.pucv.cl>
2018-05-18 22:46:31 -0300

    feat: Se pintan las reglas en las columnas de la matriz

--- src/nono/main.java
+++ src/nono/main.java
@@ -20,17 +20,13 @@ public class main {
 		//Array List de Columnas
 		ArrayList<ArrayList<Integer>> columnas = new ArrayList<ArrayList<Integer>>();
 		
-
-		//SIMULACIONES DE FILA Y CUADRADOS A PINTAR
-		char [] arrayFila = {'-' , '-' , '-' , '-' , '-'};
-		int [] arraySegmentos = {3,1};
 		
-		//ARREGLOS DE PRUEBA (Insertar en ArrayList de Filas)
+		//ARREGLOS DE PRUEBA (Insertar en ArrayList de Filas o columnas)
 		int [] arrayF1 = {1,1,1};
 		int [] arrayF2 = {1,3};
 		int [] arrayF3 = {3,1};
 		int [] arrayF4 = {5};
-		int [] arrayF5 = {1};
+		int [] arrayF5 = {1,1,1};
 
 		
 		//LLENA MATRIZ CON CARACTERES '-' (SIN PINTAR) 
@@ -57,14 +53,18 @@ public class main {
 			
 		}
 		
-		//INICIALIZAN ARRAYS LIST DE SEGMENTOS DE FILAS
+		//SE INICIALIZAN ARRAYS LIST DE SEGMENTOS DE FILAS Y COLUMNAS
 		for(int i=0; i<N; i++)
 		{	
-			filas.add(new ArrayList <Integer>() );			
+			filas.add(new ArrayList <Integer>() );	
+			columnas.add(new ArrayList <Integer>() );	
 		}
+		
 		
 		//SE LLENAN ARRAYS LIST DE SEGMENTOS DE FILAS
 		//(Debiese ser dinamico y leido desde archivo de texto)
+		
+		/*
 	
 		for(int j=0; j<arrayF1.length ; j++)
 			filas.get(0).add(arrayF1[j]);
@@ -76,6 +76,18 @@ public class main {
 			filas.get(3).add(arrayF4[j]);
 		for(int j=0; j<arrayF5.length ; j++)
 			filas.get(4).add(arrayF5[j]);
+		*/
+		
+		for(int j=0; j<arrayF1.length ; j++)
+			columnas.get(0).add(arrayF1[j]);
+		for(int j=0; j<arrayF2.length ; j++)
+			columnas.get(1).add(arrayF2[j]);
+		for(int j=0; j<arrayF3.length ; j++)
+			columnas.get(2).add(arrayF3[j]);
+		for(int j=0; j<arrayF4.length ; j++)
+			columnas.get(3).add(arrayF4[j]);
+		for(int j=0; j<arrayF5.length ; j++)
+			columnas.get(4).add(arrayF5[j]);
 			
 		
 	
@@ -92,9 +104,21 @@ public class main {
 
 		}
 		
+		System.out.println("\nARRAY LIST COLUMNAS: ");
+		for(int i=0; i<columnas.size(); i++)
+		{	
+			System.out.print(" \n--> AL "+(i+1)+": ");
+			for(int j=0; j<columnas.get(i).size() ; j++)
+			{
+				System.out.print(" "+ columnas.get(i).get(j)); 
+			}
+
+		}
+		
 		
 		//CICLO PARA FILAS
 		
+		/*
 		for(int x=0 ; x<filas.size() ; x++)
 		{
 			int suma = 0;
@@ -137,6 +161,60 @@ public class main {
 					{
 						matriz[x][indiceColumna]='-';
 						indiceColumna++;
+					}
+					
+				}
+				
+				
+			}
+			
+		}
+		*/
+		
+		//CICLO PARA COLUMNAS
+		
+		for(int x=0 ; x<columnas.size() ; x++)
+		{
+			int suma = 0;
+			
+			//SUMA DE CUADRADOS A PINTAR EN COLUMNA
+			for(int i=0; i<columnas.get(x).size(); i++)
+			{
+				suma = suma + columnas.get(x).get(i);
+			}
+			
+			//PINTAR REGLA 1 EN COLUMNA	
+			if(columnas.get(x).size()==1 && columnas.get(x).get(0) == N)
+			{
+				for(int i=0; i<columnas.get(x).size(); i++)
+				{
+					matriz[i][x] = '#';
+				}
+					
+			}
+			
+			//PINTAR REGLA 2 EN COLUMNA
+			if(suma == (N-(columnas.get(x).size()-1)))
+			{
+				
+				int indiceFila = 0;
+				
+				for(int i=0; i<columnas.get(x).size(); i++)
+				{
+					int segmento = columnas.get(x).get(i);
+					
+					for(int j=0; j<segmento; j++)
+					{
+						//PINTA CON '#'
+						matriz[indiceFila][x]='#';
+						indiceFila++;
+					}
+					
+					//AGREGAR ESPACIO
+					if(i<columnas.get(x).size()-1)
+					{
+						matriz[indiceFila][x]='-';
+						indiceFila++;
 					}
 					
 				}
